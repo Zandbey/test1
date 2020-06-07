4dd0ea2b7a8273eca1cc349616aac4d364bd9562
Zandbey <2279683584@qq.com>
2020-06-07 22:27:40 +0800

    2020.6.7提交

---
+++ 4.2实验3/4.2实验3/4.2实验3View.h
@@ -0,0 +1,46 @@
+
+// 4.2实验3View.h : CMy42实验3View 类的接口
+//
+
+#pragma once
+
+
+class CMy42实验3View : public CView
+{
+protected: // 仅从序列化创建
+	CMy42实验3View();
+	DECLARE_DYNCREATE(CMy42实验3View)
+
+// 特性
+public:
+	CMy42实验3Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMy42实验3View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 4.2实验3View.cpp 中的调试版本
+inline CMy42实验3Doc* CMy42实验3View::GetDocument() const
+   { return reinterpret_cast<CMy42实验3Doc*>(m_pDocument); }
+#endif
+
